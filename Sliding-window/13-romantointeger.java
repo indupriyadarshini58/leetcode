75a42b176d4299e81f2b80ca5e1672d55ebf17ac
indupriyadarshini58 <indupriyadarshini8@gmail.com>
2026-02-05 19:40:22 +0530

    1752-arrayissortedornot

---
+++ Sliding-window/13-romantointeger.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int romanToInt(String s) {
+        int result = 0;
+        for (int i = 0; i < s.length() - 1; i++) {
+            int current = value(s.charAt(i));
+            int next = value(s.charAt(i + 1));
+            if (current < next) {
+                result -= current;
+            } else {
+                result += current;
+            }
+        }
+        result += value(s.charAt(s.length() - 1));
+          return result;
+    }
+    private int value(char c) {
+        switch (c) {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+        }
+        return 0;
+        
+    }
+}
